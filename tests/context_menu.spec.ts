5d573bfa3301ded236bf871d76ab7602bdb2c563
Siyavush Nazhmudinov <siyavush@MacBook-Pro-admin-2.local>
2024-12-31 10:42:43 +0500

    used assertion in context menu test

--- tests/context_menu.spec.ts
+++ tests/context_menu.spec.ts
@@ -1,10 +1,10 @@
-import {test, expect, Locator} from '@playwright/test';
+import {test, Locator, expect} from '@playwright/test';
 
 test('Context Menu', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/context_menu');
     
-    // In order to modify default playwright dialog handling , we need to add a listener to the page
-    page.on('dialog', dialog => console.log(dialog.message()));
+    // In order to modify default playwright dialog handling , we need to add a listener to the page and perform the assertion before clicking
+    page.on('dialog', dialog => expect(dialog.message()).toContain('You selected a context menu'));
     page.on('dialog', dialog => dialog.accept());
 
     // Perfom the right click on the box
